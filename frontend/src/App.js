a9cf3a1aa3fb01c92c5b3d215ebbb09ddbb89ce5
Gabriel Lee <gabrielleeyj@outlook.com>
2021-02-04 22:26:43 +0800

    dashboard and login

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import SignIn from "./components/LoginView/Login";
+// <SignIn />
+import Dashboard from "./components/DashboardView/Dashboard";
+function App() {
+	return (
+		<div className="App">
+			<Dashboard />
+		</div>
+	);
+}
+
+export default App;
