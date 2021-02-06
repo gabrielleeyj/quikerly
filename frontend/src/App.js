b4237b7ed6b63402d0eaf5cb31b29f0ddcefb9bc
Gabriel Lee <gabrielleeyj@outlook.com>
2021-02-06 12:51:28 +0800

    include firebase auth code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
 // import SignIn from "./components/LoginView/Login";
 // <SignIn />
-import Dashboard from "./components/DashboardView/Dashboard";
+// import Dashboard from "./components/DashboardView/Dashboard";
+// import config from "./components/Firebase/Config";
+// import {
+// 	FirebaseAuthProvider,
+// 	FirebaseAuthConsumer,
+// } from "@react-firebase/auth";
+
 function App() {
 	return (
 		<div className="App">
+			{/* <FirebaseAuthProvider {...config} firebase={firebase}>
+				<FirebaseAuthConsumer>
+					{({ isSignedIn }) => {
+						if (isSignedIn === true) {
+							return <Dashboard credentials={credentials} />;
+						} else {
+							return "Invalid User";
+						}
+					}}
+				</FirebaseAuthConsumer>
+			</FirebaseAuthProvider> */}
 			<Dashboard />
 		</div>
 	);
---
+++ frontend/src/components/Firebase/Config.js
@@ -0,0 +1,9 @@
+const config = {
+	apiKey: "API_KEY",
+	projectId: "PROJECT_ID",
+	databaseURL: "DATABASE_URL",
+	authDomain: "AUTH_DOMAIN",
+	// OPTIONAL
+	storageBucket: "STORAGE_BUCKET",
+	messagingSenderId: "MESSAGING_SENDER_ID",
+};
