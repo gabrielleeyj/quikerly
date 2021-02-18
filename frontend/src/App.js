eda7e2151e1686abbe6e21176c4c3f4893f9fa67
Gabriel Lee <gabrielleeyj@outlook.com>
2021-02-18 22:34:58 +0800

    commit latest frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,70 @@
-import "react-perfect-scrollbar/dist/css/styles.css";
+import "./App.css";
 import React, { useState } from "react";
-import { useRoutes } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core";
-import GlobalStyles from "./components/GlobalStyles";
-import "./mixins/chartjs";
-import theme from "./theme";
-import routes from "./routes";
+import LandingPage from "./components/WelcomeView/LandingPage";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import SignUp from "./components/WelcomeView/SignUp";
+import SignIn from "./components/WelcomeView/SignIn";
+import ForgotPassword from "./components/WelcomeView/ForgotPassword";
+import Dashboard from "./components/DashboardView/Dashboard";
+import Customers from "./components/DashboardView/Customers";
+import Orders from "./components/DashboardView/Orders";
+import Profile from "./components/ProfileView/Profile";
 import fire from "./firebase/Config";
+// import {
+// 	FirebaseAuthProvider,
+// 	FirebaseAuthConsumer,
+// } from "@react-firebase/auth";
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  fire.auth().onAuthStateChanged(user => {
-    return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
-  });
-  const routing = useRoutes(routes(isLoggedIn));
-  console.log("logged in?", isLoggedIn);
+function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	fire.auth().onAuthStateChanged((user) => {
+		return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+	});
 
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {routing}
-    </ThemeProvider>
-  );
-};
+	console.log("logged in?", isLoggedIn);
+
+	return (
+		<div className="App">
+			<Router>
+				{!isLoggedIn ? (
+					<>
+						<Switch>
+							<Route
+								exact
+								from="/"
+								render={(props) => <LandingPage {...props} />}
+							/>
+							<Route
+								exact
+								path="/sign-up"
+								render={(props) => <SignUp {...props} />}
+							/>
+							<Route
+								exact
+								path="/sign-in"
+								render={(props) => <SignIn {...props} />}
+							/>
+							<Route
+								exact
+								path="/forgot-password"
+								render={(props) => <ForgotPassword {...props} />}
+							/>
+						</Switch>
+					</>
+				) : (
+					<>
+						<Link to="/dashboard" component={Dashboard} />
+						<Switch>
+							<Route path="/dashboard" component={Dashboard} />
+							<Route path="/orders" component={Orders} />
+							<Route path="/customers" component={Customers} />
+							<Route path="/profile:id" component={Profile} />
+						</Switch>
+					</>
+				)}
+			</Router>
+		</div>
+	);
+}
 
 export default App;
--- frontend/src/firebase/Config.js
+++ frontend/src/firebase/Config.js
@@ -1,46 +1,20 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqrFY2XX0Ys92lxNqwmHt5FgqcMVl6-BI",
-  authDomain: "quikerly-auth.firebaseapp.com",
-  projectId: "quikerly-auth",
-  storageBucket: "quikerly-auth.appspot.com",
-  messagingSenderId: "84797213970",
-  appId: "1:84797213970:web:f37208d0abe669b431b7cb"
+	apiKey: "AIzaSyCqrFY2XX0Ys92lxNqwmHt5FgqcMVl6-BI",
+	authDomain: "quikerly-auth.firebaseapp.com",
+	projectId: "quikerly-auth",
+	storageBucket: "quikerly-auth.appspot.com",
+	messagingSenderId: "84797213970",
+	appId: "1:84797213970:web:f37208d0abe669b431b7cb",
 };
 
 try {
-  firebase.initializeApp(firebaseConfig);
+	firebase.initializeApp(firebaseConfig);
 } catch (err) {
-  if (!/already exists/.test(err.message)) {
-    console.error("Firebase initialization error", err.stack);
-  }
+	if (!/already exists/.test(err.message)) {
+		console.error("Firebase initialization error", err.stack);
+	}
 }
 const fire = firebase;
 export default fire;
-
-const googleProvider = new firebase.auth.GoogleAuthProvider();
-export const signInWithGoogle = () => {
-  fire
-    .auth()
-    .signInWithPopup(googleProvider)
-    .then(res => {
-      console.log(res.user);
-    })
-    .catch(error => {
-      console.log(error.message);
-    });
-};
-
-const facebookProvider = new firebase.auth.FacebookAuthProvider();
-export const signInWithFacebook = () => {
-  fire
-    .auth()
-    .signInWithPopup(facebookProvider)
-    .then(res => {
-      console.log(res.user);
-    })
-    .catch(error => {
-      console.log(error.message);
-    });
-};
