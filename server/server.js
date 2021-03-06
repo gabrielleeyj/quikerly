91ca5c9cbdb84cf6e5ea84e0f86cd2de0177cccd
Gabriel Lee <gabrielleeyj@outlook.com>
2021-03-06 12:26:14 +0800

    path change

--- server/server.js
+++ server/server.js
@@ -37,10 +37,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/orders/", routes);
 
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname + "../build"));
 
 app.get("/*", (req, res) => {
-	res.sendFile(__dirname + "/build/index.html");
+	res.sendFile(__dirname + "../build/index.html");
 });
 
 const server = http.createServer(app);
